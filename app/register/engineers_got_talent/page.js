580fe066b3c4cbc027773badbfaedfde86188296
abhay2133 <abhaybishthestudent@gmail.com>
2024-09-04 17:06:43 +0530

    hahahhaha

--- app/register/engineers_got_talent/page.js
+++ app/register/engineers_got_talent/page.js
@@ -1,46 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
-export default function Register() {
-  const [infoTheme, setInfoTheme] = useState("suggest");
-  const [infoText, setInfoText] = useState(" * fields are required");
-
-  const otherDepFee = 50;
-  const [formData, setFormData] = useState({ transition_amount: otherDepFee });
+export default function BGMI() {
+  const [formData, setFormData] = useState({});
   const onSubmit = async (e) => {
     e.preventDefault();
-    setInfoTheme("suggest");
-    setInfoText('Uploading Form ...')
-
-    console.log({ formData });
-    try {
-      const response = await fetch("/api/registration", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ ...formData, year: parseInt(formData.year) }),
-      });
-
-      if (response.ok) {
-        let data = await response.json();
-        console.log({ data });
-        setInfoTheme("success")
-        setInfoText("Form Submitted Successfully");
-      } else {
-        console.error("Failed to submit form");
-        setInfoTheme("error");
-        setInfoText("Error submitting form");
-        // Handle the error (e.g., show an error message)
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      setInfoTheme("error");
-      setInfoText("Error submitting form");
-      // Handle the error (e.g., show an error message)
-    }
+    console.log(formData);
   };
 
   const handleInput = (e) => {
@@ -48,13 +15,15 @@ export default function Register() {
     setFormData({ ...formData, [name]: value });
   };
 
+
   return (
     <div className="w-full grid-bg text-white py-10">
       <form
         onSubmit={onSubmit}
-        className="bg-blur w-[95%] mx-auto md:w-[600px] flex flex-col bg-[rgba(255,255,255,0.1)] px-10 rounded-3xl border border-gray-500"
+        className="bg-blur w-[95%] mx-auto md:w-[600px] flex flex-col bg-[rgba(255,255,255,0.1)] px-10 rounded-3xl"
       >
         <h1 className="text-center text-2xl md:4xl py-10">
+          - Engineers' Got Talent -<br />{" "}
           <span className="text-gray-400">Registration Form</span>
         </h1>
         {/* Team Name - Leader */}
@@ -77,25 +46,23 @@ export default function Register() {
           />
         </div>
         {/* Player 2 - player 3 */}
-
+      
         {/* Year - Semester */}
         <div className="flex flex-col md:flex-row gap-x-5">
-          <Select
-            options={["1st", "2nd", "3rd", "4th", "5th"]}
+        <Select
             label={"Year"}
             name={"year"}
+            options={["1st"]}
+            className={"flex-1"}
             onChange={handleInput}
-            value={formData["year"]}
-            className="flex-1"
             required
           />
-          <Select
-            options={Array.from({ length: 10 }).map((e, i) => i + 1)}
+        <Select
             label={"Semester"}
             name={"semester"}
+            options={["1st"]}
+            className={"flex-1"}
             onChange={handleInput}
-            value={formData["semester"]}
-            className="flex-1"
             required
           />
         </div>
@@ -120,7 +87,7 @@ export default function Register() {
           />
         </div>
 
-        {/* Phone - email */}
+        {/* Phone - Department */}
         <div className="flex flex-col md:flex-row gap-x-5">
           <Input
             label={"Email"}
@@ -133,7 +100,7 @@ export default function Register() {
           />
           <Input
             className={"flex-1"}
-            type="number"
+            type="phone"
             label={"Phone"}
             value={formData["phone"]}
             onChange={handleInput}
@@ -141,8 +108,6 @@ export default function Register() {
             required
           />
         </div>
-
-        {/* Department - Branch */}
         <div className="flex flex-col md:flex-row gap-x-5">
           <Select
             options={departments}
@@ -159,67 +124,32 @@ export default function Register() {
             value={formData["branch"]}
             onChange={handleInput}
             name="branch"
-            placeholder={"eg. Core"}
+            placeholder={'eg. Core'}
             required
           />
-        </div>
-
-        {/* Event selectoin */}
-        <div className="flex flex-col md:flex-row gap-x-5">
+          </div>
+          <div className="flex flex-col md:flex-row gap-x-5">
           <Select
             options={events}
-            label={"Select an Event"}
-            name={"event"}
+            label={"Events"}
+            name={"events"}
             onChange={handleInput}
-            value={formData["event"]}
+            value={formData["events"]}
             className="flex-1"
             required
           />
-        </div>
-
-        {/* Price - ScreenShot */}
-        <div className="flex flex-col md:flex-row gap-x-5">
-          <div className="flex-1 flex-shrink mb-2">
-            <div className="text-white mb-2">Charges</div>
-            <div className="text-gray-300 flex-1 h-[40px] border flex items-center justify-center border-[#444] rounded text-center">
-              {formData["department"]
-                ? formData["department"] === "UIT"
-                  ? "FREE"
-                  : `Rs. ${otherDepFee}`
-                : "Choose a Department first"}
-            </div>
-          </div>
           <div className="flex-1"></div>
-        </div>
+          </div>
+        {/* Price - ScreenShot */}
+       
 
         {/* Payment screenshot */}
-        <div className="flex flex-col mt-2">
-          {formData["department"] && formData["department"] != "UIT" && (
-            <>
-              <div className="mb-2">Scan this QR and Upload the Screenshot</div>
-              <img
-                src="/payment-qr.jpg"
-                className="w-full md:w-[50%] mx-auto rounded"
-              />
-              <Input
-                className={"mt-3"}
-                label={"Transaction ID"}
-                name={"transaction_id"}
-                value={formData["transaction_id"]}
-                onChange={handleInput}
-                required
-              />
-            </>
-          )}
-        </div>
-
-        <Info theme={infoTheme} onClose={()=>setInfoTheme('none')} text={infoText}  />
+        
         {/* <InfoBox theme="error" />
         <InfoBox theme="success" /> */}
-
         {/* SUbmit button */}
-        <div className="my-4 flex">
-          <button className="hover:bg-[#223] px-10 py-3 border-gray-700 border bg-[#00000088] text-white rounded-lg flex-1 md:flex-none  focus:outline-none focus:ring-[3px] focus:ring-blue-600 focus:border-transparent active:scale-90 transition-none ">
+        <div className="my-4 flex ">
+          <button className="hover:bg-[#223] px-10 py-3 border-none bg-[#00000088] text-white rounded-lg flex-1 md:flex-none  focus:outline-none focus:ring-[3px] focus:ring-blue-600 focus:border-transparent active:scale-90 transition-none ">
             Submit
           </button>
         </div>
@@ -380,16 +310,8 @@ function Select({
   );
 }
 
-function InfoBox({ _theme = "none", text = "" }) {
-  const [theme, setTheme] = useState(_theme);
-  console.log("theme")
-  useEffect(() => {
-    setTheme(_theme);
-  }, [_theme]);
-  return <Info theme={theme} text={text} onClose={() => setTheme("none")} />;
-}
 
-function Info({
+function InfoBox({
   theme = "none" || "error" || "alert" || "success",
   text = "Info Box",
   onClose = () => {},
@@ -416,9 +338,6 @@ function Info({
     case "alert":
       style = createStyle("flex", "#edff64", "#000");
       break;
-    case "suggest":
-      style = createStyle("flex", "royalblue", "#fff");
-      break;
     case "success":
       style = createStyle("flex", "#228844", "#fff");
       break;
@@ -426,7 +345,7 @@ function Info({
   return (
     <div className="my-1 flex items-center rounded pr-3 py-2" style={style}>
       <div className="flex-1 px-4 text-wrap break-all">{text}</div>
-      <div className=" text-xs cursor-pointer" onClick={onClose}>
+      <div className=" text-xs" onClick={onClose}>
         ❌
       </div>
     </div>
@@ -438,33 +357,13 @@ const cycle = (n, a, z) => {
   if (n > z) return a;
   return n;
 };
-const departments = [
-  "UIT",
-  "USCS",
-  "SALS",
-  "SLA",
-  "UIPS",
-  "LCD",
-  "UIM",
-  "USHHM",
-  "USJMC",
-  "UCN",
-  "USHS",
-  "SOA",
-];
-
-const events = [
-  "Face Painting",
-  "Robo Race",
-  "Website making competition",
-  "Boat Race",
-  "Painting",
-  "Engineers' Day Fun Run",
-  "Technical Quiz Competition",
-  "Video Creation",
-  "Model Presentation",
-  "Cosplay",
-  "CODING WAR",
-  "Anime creation",
-  "Cook Without Fire",
-];
+const departments=[
+  "UIT"
+]
+const events=[
+  "Dancing",
+  "Singing",
+  "Poetry",
+  "Mimicry",
+  "Rapping"
+]
